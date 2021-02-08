be1f15752f7979fa3ea47936ce9b2a32d8444799
Cindy <syhndie2@gmail.com>
2021-02-08 10:18:02 -0600

    submitted message converted to base64 characters and displayed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('hey');
+document.querySelector('form').addEventListener('submit', event => {
+    //keeps the browser from attempting to submit to a server
+    event.preventDefault();
+
+    const input = document.querySelector('#message-input');
+    const encrypted = btoa(input.value);
+
+    document.querySelector('#link-input').value = encrypted;
+
+});
