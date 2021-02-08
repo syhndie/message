f772a60d6e5b2d1852e4a07be902416bdaba0214
Cindy <syhndie2@gmail.com>
2021-02-08 10:54:51 -0600

    message link hidden until submit when message input becomes hidden

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ document.querySelector('form').addEventListener('submit', event => {
     //keeps the browser from attempting to submit to a server
     event.preventDefault();
 
+    document.querySelector('#message-form').classList.add('hide');
+    document.querySelector('#link-form').classList.remove('hide');
     const input = document.querySelector('#message-input');
     const encrypted = btoa(input.value);
 
