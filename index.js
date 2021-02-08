372c92eb29d43529e321c22947c0a5b2295c5381
Cindy <syhndie2@gmail.com>
2021-02-08 10:34:43 -0600

    base64 message appended to local window url as hashtag fragment

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ document.querySelector('form').addEventListener('submit', event => {
     const input = document.querySelector('#message-input');
     const encrypted = btoa(input.value);
 
-    document.querySelector('#link-input').value = encrypted;
+    const linkInput = document.querySelector('#link-input');
+    linkInput.value = `${window.location}#${encrypted}`;
+    linkInput.select();
 
 });
